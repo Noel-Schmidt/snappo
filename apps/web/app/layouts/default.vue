c470e803f9d75f3eea459204a734e0f73b104a39
noelschmidt <noel_schmidt@outlook.de>
2025-09-16 22:14:12 +0200

    feat: added app footer

--- apps/web/app/layouts/default.vue
+++ apps/web/app/layouts/default.vue
@@ -2,7 +2,10 @@
   <app-navigation />
 
   <slot />
+
+  <app-footer />
 </template>
 <script setup lang="ts">
 import AppNavigation from "~/components/navigation/appNavigation.vue";
+import AppFooter from "~/components/footer/appFooter.vue";
 </script>
