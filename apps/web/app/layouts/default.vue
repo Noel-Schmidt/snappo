29de25766f1be1fe0e6a96eb971691c295c62054
noelschmidt <noel_schmidt@outlook.de>
2025-09-16 21:34:48 +0200

    feat: added app navigation

---
+++ apps/web/app/layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <app-navigation />
+
+  <slot />
+</template>
+<script setup lang="ts">
+import AppNavigation from "~/components/navigation/appNavigation.vue";
+</script>
--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,15 +1,25 @@
 import tailwindcss from '@tailwindcss/vite'
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
 
     devtools: { enabled: true },
 
+    app: {
+        head: {
+            htmlAttrs: {
+                class: 'dark'
+            }
+        }
+    },
+
     css: ['~/assets/css/tailwind.css'],
 
     vite: {
         plugins: [
             tailwindcss(),
+            tsconfigPaths()
         ],
     },
 
